0bca91dfbe6cd0c6250972635c503ed35e8efc19
Samiksha Dadaji Gabhane <samiksha.gabhane@valuelabs.com>
2022-11-04 17:40:50 +0530

    compress pickle changes

--- main/test.py
+++ main/test.py
@@ -2,7 +2,8 @@ import pandas as pd
 from sklearn.datasets import load_boston
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-import pickle as pkl
+#import pickle as pkl
+from compress_pickle import dump
 import os
 
 
@@ -22,11 +23,18 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.30, random_state=42)
 
 
+
+
 ## standardizing the dataset
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 X_train=scaler.fit_transform(X_train)
 X_test=scaler.transform(X_test)
+
+
+test_data = pd.DataFrame(X_test)
+# saving the dataframe
+test_data.to_csv(r'./linear_regression_testfile.csv')
 
 
 regression=LinearRegression()
@@ -35,7 +43,7 @@ regression.fit(X_train,y_train)
 
 print("creating pkl file")
 print(os.getcwd())
-pkl.dump(regression,open('./model.pkl','wb'))
+dump(regression,'./model.pkl',compression="lzma",set_default_extension=False)
 
 
 reg_pred=regression.predict(X_test)
